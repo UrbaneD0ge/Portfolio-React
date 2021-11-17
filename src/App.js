d203c9d333f3774d8817f381e4bf00ecc8f9824b
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2021-11-16 19:29:36 -0500

    Reaccify
    Co-authored-by: Handan12 <Handan12@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import AboutMe from './components/aboutMe';
+import Work from './components/work';
+import { useState } from 'react';
 
 function App() {
+  const [currentSection, getCurrentSection] = useState(AboutMe);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="App">
+      <nav>
+          <header>
+              <h1>Kipling C. Dunlap</h1>
+          </header>
+          <ul class="right">
+              <li><a href="#AboutMe">About Me</a></li>
+              <li><a href="#Work">Work</a></li>
+              <li><a href="#ContactMe">Contact Me</a></li>
+              <li><a href="https://docs.google.com/document/d/1vkpMSKmEacOYjhZEVyZuvSriz0JI9MmWvxPNrwSwGog/edit?usp=sharing" target="_blank" rel="noreferrer">Resume</a></li>
+        </ul>
+      </nav>
+      <img src="./assets/simit cover.jpg" alt="cover" id="cover"/>
+      <getCurrentSection currentSection={AboutMe} getCurrentSection={getCurrentSection} />
+    </body>
   );
 }
 
---
+++ src/components/aboutMe.js
@@ -0,0 +1,11 @@
+function AboutMe() {
+    return (
+        <section id="AboutMe" class="About Me">
+            <h2>About Me</h2>
+            <p> Born and raised in the Atlanta, GA area; Kip holds a Bachelor of Science degree in Public Policy Analysis from Georgia State University, and is presently attending Georgia Tech's "Full Stack Web Developer" bootcamp with an anticipated graduation date in December of 2021.
+            </p>
+        </section>
+    );
+}
+
+export default AboutMe;
