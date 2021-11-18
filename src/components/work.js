26ef8e4f1fc83995e6810c3d1c282da63934268d
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2021-11-17 19:39:58 -0500

    contact me dflex

--- src/components/work.js
+++ src/components/work.js
@@ -3,28 +3,28 @@ function Work() {
         <main>
             <section id="Work" class="work">
                 <h2>Work</h2>
-                <div class="card">
-                    <a href="https://urbaned0ge.github.io/Musical-Time-Machine/" target="_blank" rel="noreferrer"><img src="./MTM.png" alt="Project1" id="hero"/></a>
+                <div class="main">
+                    <a href="https://urbaned0ge.github.io/Musical-Time-Machine/" target="_blank" rel="noreferrer"><img src="./assets/MTM.png" alt="Project1" id="hero"/></a>
                     <h3><a href="https://github.com/UrbaneD0ge/Musical-Time-Machine" target="_blank" rel="noreferrer">The MUSICAL Time Machine</a></h3>
                     <h4>Multiple API calls take you back in time to find popular music of the day...</h4>
                 </div>
                 <div class="card">
-                    <a href="https://group-event-planner.herokuapp.com/login" target="_blank" rel="noreferrer"><img src="./PartyPlannerss.png" alt="PartyPlanner" class="small"/></a>
+                    <a href="https://group-event-planner.herokuapp.com/login" target="_blank" rel="noreferrer"><img src="./assets/PartyPlannerss.png" alt="PartyPlanner" class="small"/></a>
                     <h3><a href="https://github.com/UrbaneD0ge/event-planner" target="_blank" rel="noreferrer">Party Planner</a></h3>
                 <h4>Full-stack web app for creating events and managing RSVPs</h4>
                 </div>
                 <div class="card">
-                    <a href="https://urbaned0ge.github.io/JS-Password-Generator/" target="_blank" rel="noreferrer"><img src="./PWgen.png" href="https://urbaned0ge.github.io/JS-Password-Generator/" alt="Project2" class="small"/></a>
+                    <a href="https://urbaned0ge.github.io/JS-Password-Generator/" target="_blank" rel="noreferrer"><img src="./assets/PWgen.png" href="https://urbaned0ge.github.io/JS-Password-Generator/" alt="Project2" class="small"/></a>
                     <h3><a href="https://github.com/UrbaneD0ge/JS-Password-Generator" target="_blank" rel="noreferrer">JS PW Gen</a></h3>
                     <h4>Generate a randomized password based on user-selected parameters..</h4>
                 </div>
                 <div class="card">
-                    <a href="https://urbaned0ge.github.io/HW5-Work-Planner/" target="_blank" rel="noreferrer"><img src="./Planner.png" href="https://urbaned0ge.github.io/HW5-Work-Planner/" alt="Project3" class="small"/></a>
+                    <a href="https://urbaned0ge.github.io/HW5-Work-Planner/" target="_blank" rel="noreferrer"><img src="./assets/Planner.png" href="https://urbaned0ge.github.io/HW5-Work-Planner/" alt="Project3" class="small"/></a>
                     <h3> <a href="https://github.com/UrbaneD0ge/HW5-Work-Planner" target="_blank" rel="noreferrer">Day Planner</a></h3>
                     <h4>Utilizing Moment.js to create a time-aware daily planner.</h4>
                 </div>
                 <div class="card">
-                    <a href="https://urbaned0ge.github.io/Code-Quiz/" target="_blank" rel="noreferrer"><img src="./CodeQuiz.png" alt="Project4" class="small"/></a>
+                    <a href="https://urbaned0ge.github.io/Code-Quiz/" target="_blank" rel="noreferrer"><img src="./assets/CodeQuiz.png" alt="Project4" class="small"/></a>
                     <h3><a href="https://github.com/UrbaneD0ge/Code-Quiz" target="_blank" rel="noreferrer">Code Quiz</a></h3>
                     <h4>A work in progress, uses JS to test your skills!</h4>
                 </div>
