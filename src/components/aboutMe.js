758f4d5ba93e64d0a7e3fdb68351fe0c6dd30295
Kip <Kipling.Dunlap@gmail.com>
2021-12-03 10:49:44 -0500

    merge wut

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -2,7 +2,10 @@ function AboutMe() {
     return (
         <section id="AboutMe" class="About Me">
             <h2>About Me</h2>
-            <p> Born and raised in the Atlanta, GA area; Kip holds a Bachelor of Science degree in Public Policy Analysis from Georgia State University, and is presently attending Georgia Tech's "Full Stack Web Developer" bootcamp with an anticipated graduation date in December of 2021.
+            <img src="https://i.ibb.co/9twjYxJ/00100l-PORTRAIT-00100-BURST20190919181515033-COVER-2.jpg" height="300vh" alt="I'm an okra pod."/>
+            <p> Born and raised in the Atlanta, GA area; Kip holds a Bachelor of Science degree in Public Policy Analysis from Georgia State University, and received a certificate in Full-Stack Web Development coding from Georgia Tech's Bootcamp in December of 2021.
+            <br/><br/>
+                Kip spent the better part of a decade working in the Land Use and Public Transit Advocacy fields as a practitioner of Transportation Demand Management ("TDM") and was a member of <a href="https://www.actweb.org/" alt="ThreadATL" target="_blank" rel="noreferrer">Americans for Commuter Transportation</a>. He remains on the board of the Atlanta planning advocacy group, <a href="https://www.threadatl.org/" alt="ThreadATL" target="_blank" rel="noreferrer">ThreadATL</a> and serves as Chief Legislative Aide to <a href="https://www.legis.ga.gov/members/house/4880?session=1030" target="_blank" rel="noreferrer">Representative Park Cannon</a>.
             </p>
         </section>
     );
